961b1d0877efd9f5c3849bcfd6aa7096a94166d3
DESKTOP-P2A5BMQ\ongar <ext.oorynbasar@tinkoff.ru>
2021-09-25 02:13:58 +0600

    [RF] RelativeRatingCalculator refactor

--- src/main/java/kz/onggar/collector/CollectorApplication.java
+++ src/main/java/kz/onggar/collector/CollectorApplication.java
@@ -2,8 +2,10 @@ package kz.onggar.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class CollectorApplication {
 
     public static void main(String[] args) {
--- src/test/java/kz/onggar/collector/CollectorApplicationTests.java
+++ src/test/java/kz/onggar/collector/CollectorApplicationTests.java
@@ -1,13 +1,23 @@
 package kz.onggar.collector;
 
+import kz.onggar.collector.configuration.RelativeRatingConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static kz.onggar.collector.component.LobbyPlayerCount.COMMON;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CollectorApplicationTests {
+    @Autowired
+    private RelativeRatingConfiguration relativeRatingScoreDistributionConfiguration;
 
     @Test
-    void contextLoads() {
+    public void whenYamlFileProvidedThenInjectSimpleMap() {
+        assertThat(relativeRatingScoreDistributionConfiguration.getScoreDistribution().get(COMMON.name()))
+                .isNotEmpty();
+        assertThat(relativeRatingScoreDistributionConfiguration.getScoreDistribution().get(COMMON.name()))
+                .containsOnlyKeys(1, 2, 3, 4, 5, 6, 7, 8);
     }
-
 }
