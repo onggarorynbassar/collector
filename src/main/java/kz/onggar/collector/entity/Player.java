ca8ff8e42a4097688ff86da793a46acfed0ca356
Baiadil <adikmain@gmail.com>
2021-09-24 13:49:36 +0600

    [FIX] Pull request comments
    [DEL] @Column annotation
    [ADD] @Table annotation schema

--- src/main/java/kz/onggar/collector/entity/Player.java
+++ src/main/java/kz/onggar/collector/entity/Player.java
@@ -4,18 +4,18 @@ import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "player")
+@Table(name = "player", schema = "collector")
 public class Player {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
     private UUID id;
-    @Column(name = "steam_id", nullable = false)
+
     private String steamId;
-    @Column
+
     private int simpleMmr;
-    @Column
+
     private int competitiveMmr;
-    @Column
+
     private int relativeMmr;
 }
