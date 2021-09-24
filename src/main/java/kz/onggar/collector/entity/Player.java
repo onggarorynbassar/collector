1860636fc8b60dd3c0cfc868bed076610593b594
Baiadil <adikmain@gmail.com>
2021-09-24 13:40:16 +0600

    [FIX] Pull request comments
    [CHANGE] steam_id -> steamId
    [ADD] @Table annotation

--- src/main/java/kz/onggar/collector/entity/Player.java
+++ src/main/java/kz/onggar/collector/entity/Player.java
@@ -1,23 +1,21 @@
 package kz.onggar.collector.entity;
 
-
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Table(name = "player")
 public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private UUID id;
     @Column(name = "steam_id", nullable = false)
-    private String steam_id;
-    @Column(name = "simple_mmr", nullable = false)
+    private String steamId;
+    @Column
     private int simpleMmr;
-    @Column(name = "competitive_mmr", nullable = false)
+    @Column
     private int competitiveMmr;
-    @Column(name = "relative_mmr", nullable = false)
+    @Column
     private int relativeMmr;
 }
