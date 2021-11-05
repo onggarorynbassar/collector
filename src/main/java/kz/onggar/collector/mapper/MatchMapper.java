61d10b57b8bc85b73760df264ac4cf658872e4f7
DESKTOP-P2A5BMQ\ongar <ext.oorynbasar@tinkoff.ru>
2021-11-05 08:10:45 +0600

    refactor

--- src/main/java/kz/onggar/collector/mapper/MatchMapper.java
+++
@@ -1,9 +0,0 @@
-package kz.onggar.collector.mapper;
-
-import kz.onggar.collector.openapi.dto.Match;
-
-public class MatchMapper {
-//    public static Match toDto(MatchEntity matchEntity) {
-//        return new Match().id(matchEntity.id());
-//    }
-}
--- src/main/java/kz/onggar/collector/service/user/UserService.java
+++ src/main/java/kz/onggar/collector/service/user/UserService.java
@@ -1,24 +1,7 @@
 package kz.onggar.collector.service.user;
 
 import kz.onggar.collector.entity.UserEntity;
-import kz.onggar.collector.openapi.dto.User;
-
-import java.util.List;
-import java.util.UUID;
 
 public interface UserService {
-
     UserEntity createUser(String steamId);
-
-//    User createUser(User User);
-//
-//    UserEntity getUserEntityById(UUID id);
-//
-//    User getUserById(UUID id);
-//
-//    UserEntity createNewUser(String steamId);
-//
-//    List<User> getAllUsers();
-//
-//    User getUserBySteamId(String steamId);
 }
